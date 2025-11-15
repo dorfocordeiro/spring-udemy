7811cc43c6e019f1b05d1f892d564a98e6bfc49b
Alemão <270757@unifio.edu.br>
2025-11-15 11:09:18 -0300

    hello world

--- produtos-api/src/main/java/io/github/dorfocordeiro/produtos_api/ProdutosApiApplication.java
+++ produtos-api/src/main/java/io/github/dorfocordeiro/produtos_api/ProdutosApiApplication.java
@@ -8,6 +8,7 @@ public class ProdutosApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutosApiApplication.class, args);
+		System.out.println("Aplicação Produtos API iniciada com sucesso!");
 	}
 
 }
